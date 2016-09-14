54aeb2b6d914f7050f4dba0b4e48e5818cf28735
wira <wirasetiawan29@jojonomic.com>
2016-09-14 14:04:44 +0700

    Picker View
    List Picker di iOS - Swift

---
+++ LBPickerView/LBPickerView/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  LBPickerView
+//
+//  Created by Wira on 9/14/16.
+//  Copyright © 2016 Wira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        pickerData = ["Android","iOS","Windows Phone","Blackberry"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+}
+
--- LBTodoList/LBTodoList/ViewController.swift
+++ LBTodoList/LBTodoList/ViewController.swift
@@ -7,12 +7,78 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var todos = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate =
+            UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName: "Todos")
+        
+        //3
+        do {
+            let results =
+                try managedContext.executeFetchRequest(fetchRequest)
+            todos = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
+//    pass
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return todos.count
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+//        
+//        cell!.textLabel!.text = todos[indexPath.row]
+//        
+//        return cell!
+//    }
+    
+    
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        let person = todos[indexPath.row]
+        
+        cell!.textLabel!.text =
+            person.valueForKey("todo") as? String
+        
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +86,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addTodos(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Todo", message: "Add a new Todo", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default,
+                                       handler: { (action:UIAlertAction) -> Void in
+                                        
+                                        let textField = alert.textFields!.first
+                                        self.saveName(textField!.text!)
+                                        self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+            (action: UIAlertAction) -> Void in
+        }
+        
+        
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        self.presentViewController(alert,animated: true, completion: nil)
+    
+    }
+    
+    func saveName(name: String) {
+        //1
+        let appDelegate =
+            UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Todos",
+                                                        inManagedObjectContext:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+                                     insertIntoManagedObjectContext: managedContext)
+        
+        //3
+        person.setValue(name, forKey: "todo")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            todos.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
 
 }
 
